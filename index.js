e497e8502e0e65e250fd481131cf46fbeac1f439
User <ubitoz133@gmail.com>
2025-07-21 00:50:50 +0400

    finish

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const start = document.getElementById("start");
+const lap  = document.getElementById("lap");
 const stop  = document.getElementById("stop");
 const restart  = document.getElementById("restart");
+const lapArea = document.querySelector(".lap-area");
+
 
 let timeArea = document.getElementById("time-area");
+let i = 0
+
+let laps = ("");
 
 let running = false;
 let startTime = null;
@@ -12,6 +18,8 @@ let elapsedBefore = 0;
 start.addEventListener("click", () => {
   if (running) return; 
   running = true;
+  start.disabled = true
+  start.innerText = "Continue"
 
   startTime = Date.now() - elapsedBefore; 
   intervalId = setInterval(() => {
@@ -27,10 +35,29 @@ start.addEventListener("click", () => {
   }, 10);
 });
 
+lap.addEventListener("click", () => {
+    if (!running) return;
+    const elapsed = elapsedBefore;
+
+    let milliseconds = elapsed % 1000;
+    let totalSeconds = Math.floor(elapsed / 1000);
+    let seconds = totalSeconds % 60;
+    let minutes = Math.floor(totalSeconds / 60);
+
+    const lapTime = `${minutes}:${seconds}:${milliseconds}`;
+
+    const lapItem = document.createElement("div");
+    i++
+    lapItem.innerText = `Lap ${i} : ${lapTime}`;
+    lapArea.appendChild(lapItem);
+});
+
+
 stop.addEventListener("click", () => {
   if (!running) return;
   running = false;
   clearInterval(intervalId);
+  start.disabled = false
 });
 
 restart.addEventListener("click", () => {
@@ -39,4 +66,8 @@ restart.addEventListener("click", () => {
   elapsedBefore = 0;
 
   timeArea.innerHTML = "0:0:0";
+  start.innerText="Start"
+  start.disabled = false
+  lapArea.innerHTML = ""
+  i = 0
 });
